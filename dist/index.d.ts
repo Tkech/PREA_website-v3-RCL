96dee41aea0a81530346849fd90afaa560dbad3d
tkech <tkachvdmb@gmail.com>
2022-07-19 13:34:44 +0200

    new token been updated

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,9 @@
+import * as styled_components from 'styled-components';
+
 interface ButtonProps {
     label: string;
 }
 declare const Button: (props: ButtonProps) => JSX.Element;
+declare const ButtonWrapper: styled_components.StyledComponent<"div", styled_components.DefaultTheme, {}, never>;
 
-export { Button, ButtonProps };
+export { Button, ButtonProps, ButtonWrapper };
